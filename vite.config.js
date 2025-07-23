381be1ed49549a868c6ea652a815db57c8ced3f3
philipreaves <philip.reaves@gmail.com>
2025-07-23 17:10:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/cabinet-quoter/', // 👈 This must match the repo name
+  plugins: [react()]
 })
